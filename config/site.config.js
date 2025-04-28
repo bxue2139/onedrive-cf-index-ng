acf701d6b1579f4d112ab9ed02d05a5d78aa164a
Ya <72751452+bxue2139@users.noreply.github.com>
2025-04-28 09:03:36 +0800

    Update site.config.js

--- config/site.config.js
+++ config/site.config.js
@@ -49,8 +49,8 @@ module.exports = {
   // you provide. See the documentation for details.
   links: [
     {
-      name: 'GitHub',
-      link: 'https://github.com/lyc8503',
+      name: 'onedrive',
+      link: 'https://one.mios.fun/',
     },
   ],
 
